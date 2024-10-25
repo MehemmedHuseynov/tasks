a61015810f63577c52ee0415edef9e2d5c24c1a5
Mahammad <quseynovmaqa25@gmail.com>
2024-10-26 02:29:25 +0400

    finished  homework

--- Main.java
+++ Main.java
@@ -1,7 +1,40 @@
 public class Main {
-    public static void main(String[] args){
-        int number=8;
+    public static void main(String[] args) {
+        // Data type Interger
+        int number = 8;
         System.out.println(number);
-        
+        System.out.print(number + "\n");
+        System.out.printf("%s \n", number);
+        // data type Char
+        char harf = 'A';
+        System.out.println(harf);
+        System.out.print(harf + "\n");
+        System.out.printf("%s \n", harf);
+        // data type float
+        float number2 = 12;
+        System.out.println(number2);
+        System.out.print(number2 + "\n");
+        System.out.printf("%s \n", number2);
+        // data type double
+        double number3 = 12.3;
+        System.out.println(number3);
+        System.out.print(number3 + "\n");
+        System.out.printf("%s \n", number3);
+        //data type short
+        short number4 = 12222;
+        System.out.println(number4);
+        System.out.print(number4 + "\n");
+        System.out.printf("%s \n", number4);
+        //data type long
+        long number5 = 1234332342;
+        System.out.println(number5);
+        System.out.print(number5 + "\n");
+        System.out.printf("%s \n", number5);
+        // data byte byte
+        byte number6 = 121;
+        System.out.println(number6);
+        System.out.print(number6 + "\n");
+        System.out.printf("%s \n", number6);
+
     }
 }
